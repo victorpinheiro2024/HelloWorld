46a7ad61ce8afaf6cbc48a25d4d495060c1d0a6e
Victor Pinheiro <vitorsilvapinheiro@gmail.com>
2024-07-23 17:25:14 +0100

    Add files via upload
    My first script in Python.

---
+++ hello.py
@@ -0,0 +1,5 @@
+# This is my first line to print the message "Hello, World!" to the screen
+print("Hello, World!")
+
+# This line prints a quote by Fernando Pessoa to the screen, has been always with my world travelling.
+print("Life is what we make of it. Travel is the traveler. What we see isn’t what we see but what we are.” Fernando Pessoa")
